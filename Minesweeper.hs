268d3a96a0018b0b08115ff41ef4b96ce48ed0da
Jack David Turner <up776193@myport.ac.uk>
2018-12-06 16:49:01 +0100

    Added some test data

--- Minesweeper.hs
+++ Minesweeper.hs
@@ -8,22 +8,41 @@ main = do
 Definitions for developers benefit
 
 A minesweeper board size is set by the user, n. A board is therefore defined as being n tiles wide and n tiles high
+
 A tile can take multiple forms.
   - A user can "flag" a tile, this means they believe that there is a mine at this tiles location.
   - A user can select to clear a tile, if the tile is revealed to be a mine, the user loses the game.
     However, if it is not a mine, the number of mines in the surrounding tiles are shown at the selected tiles location.
     If there are not bombs in the local area, the tile must be clear.
     Surrounding tiles must then also be checked if they are clear of bombs and their surrounding tiles must be checks so on and so forth.
-Mines are placed at random locations around the board and are not revealed until the end of the game.
+
+Mines are placed at random locations around the board at the start and are not revealed until the end of the game.
+
+The user wins the game if they have selected all tiles that are not mines
+  (NOT if they have flagged all mine locations, otherwise flagging all locations would cause the user to win)
 
 -}
 
-type Flag = Char -- Placeholder here for now.
+type Flag = String
+type Board = [[Flag]]
+type Pos = (Int, Int) --column then row
 
-type Board = [[Char]]
+data Minesweeper = Minesweeper { board :: Board, boardSize :: Int, mineLocations :: [Pos]}
 
-data Minesweeper = Minesweeper { board :: Board, boardSize :: Int }
+-- Test Data
+exampleBoard :: Board
+exampleBoard = replicate exampleBoardSize (replicate exampleBoardSize "[]")
 
+exampleBoardSize :: Int
+exampleBoardSize = 9
+
+exampleMineLocations :: [Pos]
+exampleMineLocations = [(0,0),(1,0),(5,6),(7,2),(2,4),(5,1),(3,3)] -- 7 mines in total
+
+example = Minesweeper exampleBoard exampleBoardSize exampleMineLocations
+
+instance Show Minesweeper where
+  show m = "You aint done this yet"
 
 {-
 TODO
@@ -32,6 +51,8 @@ Create Minesweeper data type
 create show instance for minesweeper board
 create cell data type
 create test cases isMinesweeper etc
+create randomise bomb locations (choose difficulty to select number of bombs?)
 
 
 -}
+
