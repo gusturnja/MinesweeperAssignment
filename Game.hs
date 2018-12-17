50551b885fc8c80d09a52d2dbafb6c23f88bea53
Jack David Turner <up776193@myport.ac.uk>
2018-12-17 10:05:13 +0100

    added some comments

--- Game.hs
+++ Game.hs
@@ -4,6 +4,7 @@ import System.Random
 import Data.List
 import System.Exit
 
+-- | Create a new minesweeper and being the game loop
 main = do
   g <- newStdGen
   putStrLn "Welcome To MineSweeper!"
@@ -18,6 +19,7 @@ main = do
   gameLoop m
   putStrLn "Thanks for playing!"
 
+-- | Loop the game until the user either wins, loses or exits the game
 gameLoop :: Minesweeper -> IO ()
 gameLoop m = do
               putStrLn ""
@@ -50,6 +52,7 @@ gameLoop m = do
                       putStrLn "I'm sorry, please enter either c, f , help or exit"
                       gameLoop m
 
+-- | Take input of a coordinate, must be formatted as a position -> (y,x)
 inputCoord :: IO Pos
 inputCoord = do
               putStrLn "Please enter the coordinates (row,column)"
@@ -60,22 +63,23 @@ inputCoord = do
 -- PRINTING FUNCTIONS --------------------------------------------------------------------------------------------------
 ------------------------------------------------------------------------------------------------------------------------
 
+-- | Print a given minesweeper board with index values across the top and left side of the board
 printMinesweeper :: Minesweeper -> IO ()
 printMinesweeper m = do
-                        --print top row
-                        --print board
-                        putStrLn $ "  | " ++ printTopRow [0 .. (bsize - 1)]
+                        putStrLn $ "  | " ++ printTopRow range
                         putStrLn ("--|-" ++ concat (replicate bsize "---"))
-                        putStrLn $ printBoard $ zip [0 ..(bsize - 1)] (rows b)
+                        putStrLn $ printBoard $ zip range (rows b)
                         where
                           b = board m
                           bsize = boardSize m
+                          range = [0 .. (bsize - 1)]
 
-
+-- | Print the index, a seperator and then the board row, repeat for all rows
 printBoard :: [(Int,Row)] -> String
 printBoard [] = ""
 printBoard ((x,r):rs) = show x ++ " | " ++ show r ++ "\n" ++ printBoard rs
 
+-- | Print the top row of indexes
 printTopRow :: [Int] -> String
 printTopRow [] = ""
 printTopRow (x:xs) = " " ++ show x ++ " " ++ printTopRow xs
